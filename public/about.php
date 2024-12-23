2c986da2e1fc594d37276c88c3ea9340202408c0
batuhanerdemirsoftware <88884739+batuhanerdemirsoftware@users.noreply.github.com>
2024-12-23 23:46:07 +0300

    pixel ayarı

--- public/about.php
+++ public/about.php
@@ -4,7 +4,7 @@ $title = "About";
 include '../includes/header.php'; 
 ?>
 
-<main>
+<main class="about-main">
   <h2>About Us</h2>
   <p>We are dedicated to bringing you the latest cryptocurrency news and price updates.</p>
   <p>Our mission is to simplify the complex world of digital assets for the everyday user.</p>
